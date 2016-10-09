ab5147d6fbcc511a1a89ac6cb1f148bdbeae2d6b
Mario Castro <mcastroascencio@gmail.com>
2016-10-09 08:11:22 +0000

    first commit

---
+++ spec/models/shopping_cart_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: shopping_carts
+#
+#  id         :integer          not null, primary key
+#  status     :string
+#  ip         :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe ShoppingCart, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
